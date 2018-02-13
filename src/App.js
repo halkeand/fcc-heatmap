17501c28d65c7cfda693b8b6e57104af1d0b086e
Halkeand <Halkeand@gmail.com>
2018-02-13 11:48:41 +0100

    Fetching and basic components (Loader, section)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
+import StyledFooter from './components/StyledFooter'
+import StyledSection from './components/StyledSection'
+import Loader from './components/Loader'
 class App extends Component {
+	state = {
+		isFetching: false,
+		data: null,
+		error: null
+	}
+
+	componentDidMount() {
+		this.setState(prevState => ({
+			isFetching: true
+		}))
+
+		axios
+			.get(
+				'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json'
+			)
+			.then(resp =>
+				this.setState(prevState => ({
+					data: resp.data,
+					isFetching: false
+				}))
+			)
+			.catch(error =>
+				this.setState(prevState => ({
+					error: 'Sorry an error occured while fetching data',
+					isFetching: false
+				}))
+			)
+	}
+
 	render() {
-		return <div className="App">App</div>
+		const { isFetching, error, data } = this.state
+		console.log(data)
+		return (
+			<StyledSection>
+				{isFetching && <Loader>Fetching data</Loader>}
+				{error && <p>{error}</p>}
+				<StyledFooter />
+			</StyledSection>
+		)
 	}
 }
 

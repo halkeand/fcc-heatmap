0ac48fa547d658db95cfae4b2043c7f8df7ceda4
Halkeand <Halkeand@gmail.com>
2018-02-13 19:56:59 +0100

    Add color legend and css

---
+++ src/components/ColorLegend.js
@@ -0,0 +1,15 @@
+import React from 'react'
+export default () => (
+	<div className="legend">
+		<div className="color-legend cell-color-0">{'< 2.7'}</div>
+		<div className="color-legend cell-color-1">{'< 3.9'}</div>
+		<div className="color-legend cell-color-2">{'< 5'}</div>
+		<div className="color-legend cell-color-3">{'< 6.1'}</div>
+		<div className="color-legend cell-color-4">{'< 7.2'}</div>
+		<div className="color-legend cell-color-5">{'< 8.3'}</div>
+		<div className="color-legend cell-color-6">{'< 9.4'}</div>
+		<div className="color-legend cell-color-7">{'< 10.5'}</div>
+		<div className="color-legend cell-color-8">{'< 11.6'}</div>
+		<div className="color-legend cell-color-9">{'< 12.7'}</div>
+	</div>
+)
